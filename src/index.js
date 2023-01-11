bfb0ba1e263b0d9f2935fb5823082fc73e19fb5b
Kate Panchenko <k.panchenko12345@gmail.com>
2023-01-11 19:59:19 +0200

    done project

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function onCountrySearch() {
     fetchCountries(countryName)
         .then(countries => {
             if (countries.length > 10) {
-                Notify.info('Too many matches found. Please enter a more specific name.');
+                Notify.warning('Too many matches found. Please enter a more specific name.');
                 countryInfo.innerHTML = '';
                 countryList.innerHTML = '';
                 return;
@@ -48,17 +48,18 @@ function onCountrySearch() {
         });
 }
 
-function createMarkupCountryList({ flags, country }) {
+function createMarkupCountryList({ flags, name }) {
     return `<li class='list-item'>
-  <img class='img-item' src='${flags.svg}' alt='${country.official}' width='30' />
-  <span>${country.official}</span>
+  <img class='img-item' src='${flags.svg}' alt='${name.official}' width='30' />
+  <span>${name.official}</span>
 </li>`;  
 }
 
 function createMarkupCountryInfo({ flags, name, capital, population, languages }) {
     return `<div class="country-info__box">
-        <img class="country-info__img" src="${flags.svg}" alt="${name.official}" width='30'>
-        <h2 class="country-info__name">${name.official}</h2>
+        
+        <h2 class="country-info__name"><img class="country-info__img" src="${flags.svg}" alt="${name.official}" width='50'>
+        ${name.official}</h2>
         <p class="country-info__desc">
           <span>Capital: ${capital}</span>
         </p>
@@ -66,7 +67,7 @@ function createMarkupCountryInfo({ flags, name, capital, population, languages }
           <span>Population: ${population}</span>
         </p>
         <p class="country-info__desc">
-          <span>Languages: ${languages}</span>
+          <span>Languages: ${Object.values(languages)}</span>
         </p>
       </div>`
 }
